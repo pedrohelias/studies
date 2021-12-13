9f004bdbcc1d5f0eecd25b8aec8eb011611afcd5
pedrohelias <pedrohelias95@hotmail.com>
2021-12-13 14:02:57 -0300

    adições de javascript

---
+++ Javascript/0-intro/script.js
@@ -0,0 +1,11 @@
+console.log("hello world")
+//alert("Ola mundo") - usado para criar combobox ou alertas de sistema
+
+
+//VARIAVEIS NO JAVASCRIPT
+
+let variavelExemplo = 0;
+let variavelExemplo2 = "texto aqui"
+console.log(variavelExemplo)
+console.log(variavelExemplo2)
+
